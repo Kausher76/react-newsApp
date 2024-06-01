8886cdd9de885f570fa2397e6774a0ee0302e4be
Kausher Imam <86553740+Kausher76@users.noreply.github.com>
2024-06-02 01:00:55 +0530

    Update NewsComponent.jsx

--- src/components/NewsComponent.jsx
+++ src/components/NewsComponent.jsx
@@ -62,7 +62,7 @@ export default class NewsComponent extends Component {
       <>
         <div  style={myStyle} className="myContainer">
           <h2
-            className={`text-center my-1.5 text-${
+            className={`text-center my-2.3 text-${
               mode === "dark" ? "light" : "dark"
             }`}
           >
